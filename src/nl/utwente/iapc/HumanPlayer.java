6a487c76d51ffeb27132d426ba382ee70f48c522
Axel Vugts <mail@axelv.net>
2014-12-18 23:19:46 +0100

    Added HumanPlayer class

---
+++ src/nl/utwente/iapc/HumanPlayer.java
@@ -0,0 +1,40 @@
+package nl.utwente.iapc;
+
+public class HumanPlayer implements Player {
+
+	/**
+	 * A human player with a Name.
+	 * @author Axel Vugts
+	 *
+	 */
+	
+	private String name;
+	private int number;
+	
+	/**
+	 * A Human Player
+	 * @param name Name for the new human player
+	 * @param number Number of the new player
+	 */
+	public HumanPlayer (String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
+	
+	@Override
+	public Move nextMove() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int getPlayerNumber() {
+		return number;
+	}
+
+}
