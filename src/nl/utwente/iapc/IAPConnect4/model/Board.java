faa143efdb36ef7bfb68791f880eb81e103820b5
Axel Vugts <mail@axelv.net>
2015-01-20 12:07:14 +0100

    De-uglified Board.toString

--- src/nl/utwente/iapc/IAPConnect4/model/Board.java
+++ src/nl/utwente/iapc/IAPConnect4/model/Board.java
@@ -49,7 +49,6 @@ public class Board {
 	 * @param currentPlayer
 	 * @param playerCount
 	 */
-	
 	protected Board(int[][] board, int[] lastMove, int playerCount) {
 		this.board = board;
 		this.playerCount = playerCount;
@@ -62,7 +61,6 @@ public class Board {
 	 * @param player Player that does the move
 	 * @return BoardResult, which can be a new Board or a Board that has a winner.
 	 */
-	
 	public Board move(int move, int player) {
 		if (isLegalMove(move))
 			{
@@ -72,7 +70,7 @@ public class Board {
 				Board newBoard = new Board(board, moveDone, playerCount);
 				return newBoard;
 			} else {
-				return null;
+				throw new InvalidMoveException();
 			}
 	}
 	
@@ -201,12 +199,14 @@ public class Board {
 	
 	public String toString() {
 		String returnString = ".-.-.-.-.-.-.-.\n";
-		returnString += "|"+board[0][0]+"|"+board[0][1]+"|"+board[0][2]+"|"+board[0][3]+"|"+board[0][4]+"|"+board[0][5]+"|"+board[0][6]+"|\n";
-		returnString += "|"+board[1][0]+"|"+board[1][1]+"|"+board[1][2]+"|"+board[1][3]+"|"+board[1][4]+"|"+board[1][5]+"|"+board[1][6]+"|\n";
-		returnString += "|"+board[2][0]+"|"+board[2][1]+"|"+board[2][2]+"|"+board[2][3]+"|"+board[2][4]+"|"+board[2][5]+"|"+board[2][6]+"|\n";
-		returnString += "|"+board[3][0]+"|"+board[3][1]+"|"+board[3][2]+"|"+board[3][3]+"|"+board[3][4]+"|"+board[3][5]+"|"+board[3][6]+"|\n";
-		returnString += "|"+board[4][0]+"|"+board[4][1]+"|"+board[4][2]+"|"+board[4][3]+"|"+board[4][4]+"|"+board[4][5]+"|"+board[4][6]+"|\n";
-		returnString += "|"+board[5][0]+"|"+board[5][1]+"|"+board[5][2]+"|"+board[5][3]+"|"+board[5][4]+"|"+board[5][5]+"|"+board[5][6]+"|\n";
+		for (int i = 0; i < board.length; i++)
+		{
+			for (int j = 0; j < board[i].length; j++)
+			{
+				returnString += "|"+getField(i, j);
+			}
+			returnString += "|\n";
+		}
 		returnString += "'-'-'-'-'-'-'-'\n"; 
 		return returnString;
 	}
