cb764c8cd0d2bfa960f810eed0476a09a831df63
Axel Vugts <mail@axelv.net>
2015-01-21 15:25:04 +0100

    Started with JUnit

---
+++ src/test/BoardTest.java
@@ -0,0 +1,12 @@
+package test;
+import org.junit.Before;
+
+import nl.utwente.iapc.IAPConnect4.model.game.Board;
+public class BoardTest {
+ Board b;
+ 
+	public static void TestGetField () {
+		
+	}
+	
+}
