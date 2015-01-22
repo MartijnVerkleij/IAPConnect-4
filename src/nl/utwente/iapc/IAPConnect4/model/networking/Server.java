380d367ed35c686b0e0592b8c6608b75f2ed5750
MartijnVerkleij <mail@mverkleij.nl>
2015-01-22 21:17:36 +0100

    Server 1.0

--- src/nl/utwente/iapc/IAPConnect4/model/networking/Server.java
+++ src/nl/utwente/iapc/IAPConnect4/model/networking/Server.java
@@ -7,13 +7,13 @@ import java.util.LinkedList;
 
 import nl.utwente.iapc.IAPConnect4.controller.ClientHandler;
 import nl.utwente.iapc.IAPConnect4.controller.Game;
+import nl.utwente.iapc.IAPConnect4.util.Config;
 import nl.utwente.iapc.IAPConnect4.util.GamePoolTick;
 import nl.utwente.iapc.IAPConnect4.util.Protocol;
 
 public class Server {
 	ServerSocket ssock;
 	LinkedList<ClientHandler> clients = new LinkedList<ClientHandler>();
-	LinkedList<Game> games = new LinkedList<Game>();
 	
 	public Server(int port) {
 		try {
@@ -27,7 +27,7 @@ public class Server {
 	}
 	
 	public void startServer() {
-		GamePoolTick tick = new GamePoolTick(1, this);
+		GamePoolTick tick = new GamePoolTick(Config.GAME_POOL_TICK, this);
 		tick.start();
 		while(true) {
 			try {
@@ -63,7 +63,6 @@ public class Server {
 					" + " + notInGame.get(1).getPlayer().getName());
 			notInGame.get(0).newGame(game);
 			notInGame.get(1).newGame(game);
-			games.add(game);
 			broadcastCommand(new Command(Protocol.START_GAME, 
 					notInGame.get(0).getPlayer().getName(), 
 					notInGame.get(1).getPlayer().getName()));
@@ -82,7 +81,7 @@ public class Server {
 		return null;
 	}
 	
-	public void sendCommand(String clientName, Command c) throws NullPointerException{
+	public void sendCommand(String clientName, Command c) throws NullPointerException {
 		findClient(clientName).sendCommand(c);
 	}
 	
