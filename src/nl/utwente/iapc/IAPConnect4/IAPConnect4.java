fe668837110c7c46c12abb7cedc79e115b6ed373
Axel Vugts <mail@axelv.net>
2015-01-26 19:21:37 +0100

    Made IAPConnect4 a singleton

--- src/nl/utwente/iapc/IAPConnect4/IAPConnect4.java
+++ src/nl/utwente/iapc/IAPConnect4/IAPConnect4.java
@@ -1,9 +1,32 @@
 package nl.utwente.iapc.IAPConnect4;
 
 import nl.utwente.iapc.IAPConnect4.menu.MenuController;
+import nl.utwente.iapc.IAPConnect4.server.Server;
+import nl.utwente.iapc.IAPConnect4.server.ServerController;
 
 public class IAPConnect4 {
+	private static IAPConnect4 instance = null;
+	
+	MenuController menuController;
+	ServerController serverController;
+	protected IAPConnect4() {
+		// Safety precaution
+	}
 	public static void main(String[] args) {
-		new MenuController();
+		MenuController menuController = new MenuController();
+	}
+	
+	public static synchronized IAPConnect4 getInstance() {
+		if(instance == null) {
+	         instance = new IAPConnect4();
+         }
+		return instance;
+	}
+	public void serverMode(int port) {
+		serverController = new ServerController(port);
+	}
+	public void returnToMenu() {
+		serverController = null;
+		menuController.returnToMenu();
 	}
 }
